1d7f41f62d0caef1dba02203d8fbb6674d13f827
Kitty <the.xerx@gmail.com>
2024-12-30 20:52:05 +0200

    Hash map support for Line and Vector

--- Source/Line.h
+++ Source/Line.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <type_traits>
 #include "Vector.h"
 
 
@@ -7,3 +8,21 @@ struct SLine
 	SVector P1;
 	SVector P2;
 };
+
+
+inline bool operator==(const SLine& Line1, const SLine& Line2)
+{
+	return Line1.P1 == Line2.P1 && Line1.P2 == Line2.P2;
+}
+
+namespace std
+{
+	template<>
+	struct hash<SLine>
+	{
+		size_t operator()(const SLine& Line) const
+		{
+			return std::hash<SVector>()(Line.P1) ^ (std::hash<SVector>()(Line.P2) << 1);
+		}
+	};
+}
--- Source/Vector.h
+++ Source/Vector.h
@@ -49,9 +49,19 @@ inline SVector& operator+=(SVector& Vector1, const SVector& Vector2)
 	return Vector1;
 }
 
+inline bool operator==(const SVector& Vector1, const SVector& Vector2)
+{
+	return Vector1.X == Vector2.X && Vector1.Y == Vector2.Y;
+}
+
+inline float LengthSquared(const SVector& Vector)
+{
+	return Vector.X * Vector.X + Vector.Y * Vector.Y;
+}
+
 inline float Length(const SVector& Vector)
 {
-	return std::sqrt(Vector.X * Vector.X + Vector.Y * Vector.Y);
+	return std::sqrt(LengthSquared(Vector));
 }
 
 inline SVector Normalize(const SVector& Vector)
@@ -83,4 +93,16 @@ inline float Dot(const SVector& Vector1, const SVector& Vector2)
 inline SVector Reflect(const SVector& Vector, const SVector& Normal)
 {
 	return Vector - 2 * Dot(Vector, Normal) * Normal;
+}
+
+namespace std
+{
+	template<>
+	struct hash<SVector>
+	{
+		size_t operator()(const SVector& Vector) const
+		{
+			return std::hash<float>()(Vector.X) ^ (std::hash<float>()(Vector.Y) << 1);
+		}
+	};
 }
